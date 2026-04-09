1b4cb10172ed776ac56646084d1ff286aecac76c
Ashmer Shaikh <ashmer.s@cloudxperte.com>
2026-04-09 16:31:49 +0530

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,6 @@
 import Head from 'next/head';
 import Script from 'next/script'
-import '@fortawesome/fontawesome-free/js/fontawesome';
-import '@fortawesome/fontawesome-free/js/solid';
-import '@fortawesome/fontawesome-free/js/regular';
-import '@fortawesome/fontawesome-free/js/brands';
+import '@fortawesome/fontawesome-free/css/all.css';
 import styles from '../styles/custom.css'
 import "@/styles/chatbot.css"
 import "primeicons/primeicons.css";
